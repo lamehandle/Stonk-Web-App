53429146b2a42df6d266c52f0950043bfb0de791
lamehandle <jamesrogersgis@gmail.com>
2023-05-15 22:31:03 -0500

    WIP Routing

--- plot_app/process_stock.py
+++ plot_app/process_stock.py
@@ -7,5 +7,13 @@ default_period = "1mo"
 symbol = user_data
 stock = Stock(symbol, period=user_data['period'] | default_period, start='', end='')
 
-
-
+# variables passed to process_stock.html
+date = user_data.date
+open = user_data.open
+high = user_data.high
+low = user_data.low
+close = user_data.close
+volume = user_data.volume
+dividends = user_data.dividends
+splits = user_data.splits
+period = user_data.period | default_period
