16103827960fc001d3870eea7cc21af971f3b37d
lamehandle <jamesrogersgis@gmail.com>
2023-08-15 12:53:37 -0500

    WIP simulation loop & Controller

--- plot_app/sim_control.py
+++ plot_app/sim_control.py
@@ -1,24 +1,27 @@
 from csv_data_trans import get_symbols_from_csv
 from user import User
 
-#  take user company stock symbol
-raw_stock = input("Enter your stock symbol. ")
-raw_bank = input("How much funds are in the bank? ")
-raw_invest_amt = input("How much are you investing? ")
-# validate against list
-symbol_list = get_symbols_from_csv()
-symbol = ''
-bank = 0.0
-invest_funds = 0.0
 
-if raw_stock in symbol_list:
-    symbol = raw_stock
+class Simulation_Controller:
 
-# create new user
-user = User(bank, symbol)
+    #  take user company stock symbol
+    raw_stock = input("Enter your stock symbol. ")
+    raw_bank = input("How much funds are in the bank? ")
+    raw_invest_amt = input("How much are you investing? ")
+    # validate against list
+    symbol_list = get_symbols_from_csv()
+    symbol = ''
+    bank = 0.0
+    invest_funds = 0.0
 
-#  retreive initial history
-user.retrieve_single_day()
-# prompt user for stop loss... y/n?
+    if raw_stock in symbol_list:
+        symbol = raw_stock
+
+    # create new user
+    user = User(bank, symbol)
+
+    #  retreive initial history
+    user.retrieve_single_day()
+    # prompt user for stop loss... y/n?
 
 #
