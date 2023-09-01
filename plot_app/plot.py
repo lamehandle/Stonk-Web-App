9e7fcb9ddb003baae5c44adf27f5202cf711c0ed
lamehandle <jamesrogersgis@gmail.com>
2023-08-31 23:42:47 -0500

    WIP impl basic plotting

--- plot_app/plot.py
+++ plot_app/plot.py
@@ -1,6 +1,16 @@
 import plotly.graph_objects as go
 import plotly.express as px
 import pandas as pd
+import datetime as datetime
+
+# dataframe for plotting
+df = None
+x_axis = None
+y_axis = None
+open = None
+close = None
+low = None
+high = None
 
 
 #  x-axis should be the Date
