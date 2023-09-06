b323d9b4ba6d813f29f15525288ceba5ba907b2b
jamesr <jamesrogersgis@gmail.com>
2023-09-06 09:01:39 -0500

    WIP fixing advance time functionality     not setting end date correctly

--- plot_app/position.py
+++ plot_app/position.py
@@ -13,7 +13,8 @@ class Position:
     stop_loss_value = 0.0
     errors = {}
     position_series = None
-    period = '1d'
+    period = '3y'
+    single_day = None
 
     def __init__(self, symbol):
         self.symbol = symbol
@@ -24,6 +25,10 @@ class Position:
         hist_df = pd.DataFrame(hist).reset_index()
         self.history = hist_df
         self.position_series = self.history
+        self.strip_first_day()
+
+    def strip_first_day(self):
+        self.single_day = self.history.iloc[0]
 
     def take_profit(self, profit_value):
         if profit_value > 0.0:
@@ -36,28 +41,30 @@ class Position:
     def advance_time(self):
         # Take original Datetime from
         original_date = self.history["Date"].iloc[0]
-        current_datetime = datetime.strptime(str(original_date), "%Y-%m-%d %H:%M:%S%z")
+        # current_datetime = datetime.strptime(str(original_date), "%Y-%m-%d %H:%M:%S%z")
         print("<========= v Original Date v ========>")
         print(original_date)
 
-        next_day = (current_datetime + timedelta(days=1))
+        # next_day = self.history["Date"].iloc[0]
+        next_day = (self.history["Date"].iloc[0] + timedelta(days=1))
         # todo fix not advancing a day!
         # need to convert this to an acceptable datetime string
 
         print("<========= v New Date v ========>")
         print(next_day)
 
-        print("<========= v Original History v ========>")
-        print(self.history)
-
-        new_hist = yf.Ticker(self.symbol).history(start=next_day).reset_index()
+        # print("<========= v Original History v ========>")
+        # print(self.history)
+        # todo this is not pulling the next day, why?   --------------------vvvv
+        new_hist = yf.Ticker(self.symbol).history(start=original_date, end=next_day).reset_index()
         # think I need to format the dates
-        # hist_df = pd.DataFrame(new_hist).reset_index()
+
         print("<========= v New History v ========>")
         print(new_hist)
         # print(hist_df)
-        self.position_series = pd.concat([self.history, new_hist], axis="rows", ignore_index=True)
+        self.position_series = pd.concat([self.history.iloc[0], new_hist], ignore_index=True)
         # , axis = "columns"
+        print("<========= v Position Series v ========>")
         print(self.position_series)
         # comp_hist_df = pd.DataFrame(new_hist).reset_index()
         # self.history = comp_hist_df
