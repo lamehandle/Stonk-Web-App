954301c21764687c110a2b2e8443c3bf4e514858
lamehandle <jamesrogersgis@gmail.com>
2023-09-14 22:58:58 -0500

    WIP refactor advancing time

--- plot_app/position.py
+++ plot_app/position.py
@@ -6,28 +6,26 @@ from datetime import timedelta, datetime
 class Position:
     symbol = ""
     history = None
+    period = '1mo'
+
+    index = 0
+    initial_record = None
+    prev_date= None
+    next_day = None
+
     take_profit_value = 0.0
     stop_loss_value = 0.0
-    errors = {}
-    position_history = None
-    period = '3mo'
-    single_day = None
-    original_date = None
-    next_day = None
 
     def __init__(self, symbol):
         self.symbol = symbol
-        self.retrieve_single_day(symbol)
+        self.retrieve_history()
+        self.retrieve_initial_day()
 
-    def retrieve_single_day(self, symbol):
-        hist = yf.Ticker(symbol).history(self.period)
-        hist_df = pd.DataFrame(hist).reset_index()
-        self.history = hist_df
-        self.position_history = self.history.tail(1)
-        self.strip_first_day()
+    def retrieve_history(self):
+        self.history = yf.Ticker(self.symbol).history(self.period).reset_index()
 
-    def strip_first_day(self):
-        self.single_day = self.history.iloc[0]
+    def retrieve_initial_day(self):
+        self.initial_record = self.history.iloc[0]
 
     def take_profit(self, profit_value):
         if profit_value > 0.0:
@@ -38,28 +36,26 @@ class Position:
             self.stop_loss_value = loss_value
 
     def advance_time(self):
-        print("<========= v Original History v ========>")
-        print(self.history.head())
-
-        # Take original Datetime from history
-        # todo this if statement is not working as expected. I think I need to create a flag/counter to track days advanced and walk down the history dataframe
-        if self.original_date is not None:
-            original_date = self.original_date.head(1)
-        else:
-            original_date = self.history["Date"].head(1)
-            self.original_date = original_date
-
-        print("<========= v Original Date v ========>")
-        print(original_date)
-
-        next_day = original_date + timedelta(days=2)
+        print("<========= v History v ========>")
+        print(self.history)
+        # todo walk down the history dataframe one element at a time.
+        if self.initial_record is not None:
+            self.calc_date(self.index)
 
         print("<========= v New Date v ========>")
-        print(next_day)
+        print(self.next_day)
         #                                                 start is inclusive;   end is exclusive.
         #                                                          |                    |
-        new_range = yf.Ticker(self.symbol).history(start=original_date.iloc[0], end=next_day.iloc[0]).reset_index()
+        new_range = yf.Ticker(self.symbol).history(start=self.prev_date, end=self.next_day).reset_index()
 
         self.position_history = new_range
         print("<========= v Position Series v ========>")
         print(self.position_history)
+
+    def calc_date(self, index):
+        print("<========= v Original Date v ========>")
+        print(self.initial_record.iloc[index]["Date"])
+
+        self.prev_date = self.initial_record.iloc[self.initial_record[index], self.initial_record["Date"]]
+        self.index = self.index + 1
+        self.next_day = self.initial_record["Date"].iloc[index]
