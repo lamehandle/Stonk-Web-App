e52bffe5efbd16c916a606571d2d39943fc3bebb
James Rogers <jamesrogersgis@gmail.com>
2023-10-13 08:47:28 -0500

    WIP impl Stop/Take profit plots

--- plot_app/plot.py
+++ plot_app/plot.py
@@ -38,12 +38,12 @@ class Plot:
                     go.Scatter(x=position.history["Date"], y=position.history["Open"]))
 
                 if position.stop_loss_value:
-                    self.fig.add_hline(y=position.stop_loss_value, line_dash="dash")
+                    self.fig.add_hline(y=position.stop_loss_value, line_dash="dash", opacity=0.5, line_color="blue")
                     # self.fig.add_trace(
                     #     go.Scatter(x=position.history["Date"], y=[position.stop_loss_value]))
                 # todo refactor to provide the rows that match the value.
                 if position.take_profit_value:
-                    self.fig.add_hline(y=position.take_profit_value, line_dash="dash")
+                    self.fig.add_hline(y=position.take_profit_value, line_dash="dash", opacity=0.5, line_color="blue")
                     # self.fig.add_trace(
                     #     go.Scatter(x=position.history["Date"], y=[position.take_profit_value]))
                 # todo refactor to provide the rows that match the value.
