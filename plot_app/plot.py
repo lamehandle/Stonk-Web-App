cb25425e8a2c6e357b33c5df550ff07ab499539a
lamehandle <jamesrogersgis@gmail.com>
2023-10-15 11:23:03 -0500

    WIP tweak annotations

--- plot_app/plot.py
+++ plot_app/plot.py
@@ -22,7 +22,10 @@ class Plot:
                                                       low=position.history['Low'],
                                                       close=position.history['Close'])])
 
-            self.fig.update_layout(xaxis_rangeslider_visible=False)
+            self.fig.update_layout(xaxis_rangeslider_visible=False,
+                                   title="Historical stock data for " + position.symbol,
+                                   yaxis_title=position.symbol + "stock price",
+                                   xaxis_title="Date")
 
         else:
             return print("<======== No data! =========>")
@@ -41,7 +44,7 @@ class Plot:
                     self.fig.add_hline(y=position.stop_loss_value, line_dash="dash", opacity=0.5, line_color="blue",
                                        annotation_text="Stop Loss Order",
                                        annotation_position="top left",
-                                       annotation_font_size=18,
+                                       annotation_font_size=12,
                                        annotation_font_color="blue"
                                        )
 
@@ -62,7 +65,7 @@ class Plot:
                     self.fig.add_hline(y=position.take_profit_value, line_dash="dash", opacity=0.5, line_color="blue",
                                        annotation_text="Take Profit Order",
                                        annotation_position="top left",
-                                       annotation_font_size=18,
+                                       annotation_font_size=12,
                                        annotation_font_color="blue")
 
                     # self.fig.add_trace(
