33e5ddad3b0fc4fd1ab52591d6c0260ddb8f4acf
James Rogers <jamesrogersgis@gmail.com>
2023-10-23 16:58:57 -0500

    WIP Front End MVP

--- plot_app/csv_data_trans.py
+++ plot_app/csv_data_trans.py
@@ -9,12 +9,24 @@ def get_symbols_from_csv():
         # consume csv/excel and generate a dataframe
         comp_raw = pd.read_csv(file_path)
         print(comp_raw)
-        comp_sym = comp_raw.loc[:, ['Symbol', 'Name']]
-        print(comp_sym)
-        new_file = open("stock_symbols.txt")
-        new_file.write(comp_sym)
-        new_file.close()
 
-        return comp_sym
+        comp_filter = comp_raw.itertuples(name=None)
+
+        comp_filter = comp_raw.to_string(columns=['Symbol', 'Security Name'],
+                                         # formatters={
+                                         #     'Symbol': '('.format,
+                                         #     'Security Name': ')'.format
+                                         # },
+                                         justify="left",  header=False, index=False)
+        print(comp_filter)
+        new_file = open("stock_symbols.txt", "w")
+        print(comp_filter)
+        new_file.write(comp_filter)
+        new_file.close()
+        f = open("All_Stocks.txt", "w")
+        # return comp_filter
     else:
         print("File not found")
+
+
+get_symbols_from_csv()
